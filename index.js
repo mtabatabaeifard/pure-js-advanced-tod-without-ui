6c2bb4cff0eb0871d1d4a0935d14f6b5c40ab899
Seyyed Mohammad Hossein Tabatabaei Fard <87270258+mtabatabaeifard@users.noreply.github.com>
2022-08-31 18:11:13 +0430

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,3 @@ import { toastify } from "./modules/components/toastify.js";
 const submitTodoButton = document.getElementById("submit");
 
 submitTodoButton.addEventListener("click", handleCreateNewTodo);
-
-// const arr = ['b','c','a','d'];
-// const arr2 = [22,55,11,8];
-// console.log(arr.sort());
-// console.log(arr2.sort((a,b)=>a-b));
-// const testArr = null;
-// two methods of sorting:
-// testArr && testArr.sort();
-// testArr?.sort();
